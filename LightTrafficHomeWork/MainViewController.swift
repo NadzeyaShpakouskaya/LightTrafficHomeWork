4bb9673b131f8e0019da7475ff8edac56fa1ab3b
Nadzeya Shpakouskaya <shpakovna@gmail.com>
2021-08-17 13:55:31 +0400

    business logic of MainView Controller

--- LightTrafficHomeWork/MainViewController.swift
+++ LightTrafficHomeWork/MainViewController.swift
@@ -8,11 +8,61 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var redSignView: UIView!
+    @IBOutlet weak var yellowSignView: UIView!
+    @IBOutlet weak var greenSignView: UIView!
+    
+    @IBOutlet weak var actionButton: UIButton!
+    
+    // MARK: - Private properties
+    private var currentColorSign: SignColor?
+    
+    // MARK: - Override methods
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setUpUIElements()
     }
+    
+    // MARK: - IBActions
+    @IBAction func actionButtonPressed() {
+        actionButton.setTitle("Next", for: .normal)
+        changeSignColor()
+    }
+    
+    // MARK: - Private methods
+    fileprivate func setUpUIElements() {
+        redSignView.layer.cornerRadius = redSignView.frame.size.height / 2
+        yellowSignView.layer.cornerRadius = yellowSignView.frame.size.height / 2
+        greenSignView.layer.cornerRadius = greenSignView.frame.size.height / 2
+        
+        actionButton.layer.cornerRadius = 15
+    }
+    
+    fileprivate func changeSignColor(){
+        switch currentColorSign {
+        case .red:
+            redSignView.layer.opacity = 0.35
+            yellowSignView.layer.opacity = 1
+            currentColorSign = .yellow
+        case .yellow:
+            yellowSignView.layer.opacity = 0.35
+            greenSignView.layer.opacity = 1
+            currentColorSign = .green
+        case .green:
+            redSignView.layer.opacity = 1
+            greenSignView.layer.opacity = 0.35
+            currentColorSign = .red
+        case .none:
+            redSignView.layer.opacity = 1
+            currentColorSign = .red
+        }
+    }
+}
 
+extension MainViewController {
+    enum SignColor {
+        case red, yellow, green
+    }
 }
